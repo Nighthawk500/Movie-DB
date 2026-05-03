5036f3d1b822e1b8c2f834362800a8e0deb15f37
Josie Hunter <huntej25@ferris.edu>
2026-05-03 16:19:23 -0400

    Update ssi-header.php
    Comments

--- includes/ssi-header.php
+++ includes/ssi-header.php
@@ -1,43 +1,34 @@
 <!DOCTYPE html>
+    
+<!-- 
+Group Project - ISYS 288
+5/3/2026
+Authors:
+    Index: Josephine Hunter
+    Database: Jonathan
+    Select: Bennett
+    Delete: Chloe
+    Insert: Brandon
+    Update: Quaivion
+-->
 
-<html lang="en">
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <title>Movie Mayhem</title>
+            <link rel="stylesheet" href="../css/styles.css">
+        </head>
+        
+        <body>
+            <header class="site-header">
+                <div class="logo">
+                    Movie Mayhem
+                </div>
 
-<head>
-
-    <meta charset="UTF-8">
-
-    <title>Movie Mayhem</title>
-
-    <link rel="stylesheet" href="../css/styles.css">
-
-</head>
-
-
-
-<body>
-
-
-
-    <header class="site-header">
-
-
-
-        <div class="logo">
-
-            Movie Mayhem
-
-        </div>
-
-
-
-        <nav class="navbar">
-
-            <a href="index.php">Home</a>
-
-            <a href="movies.php">Page 2</a>
-
-            <a href="search.php">More</a>
-
-        </nav>
-
-    </header>
+                <!-- Navigation Menu -->
+                <nav class="navbar">
+                    <a href="index.php">Home</a>
+                    <a href="movies.php">Page 2</a>
+                    <a href="search.php">More</a>
+                </nav>
+            </header>
