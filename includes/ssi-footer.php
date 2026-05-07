5cebd84b0f0e19fe89512fbba83343a10b35e8b3
Quaivion Welch <quaivionw@gmail.com>
2026-05-06 21:03:44 -0400

    Update ssi-footer.php

--- includes/ssi-footer.php
+++ includes/ssi-footer.php
@@ -1,9 +1,19 @@
 <footer class="site-footer">
-    <p>&copy; <?php echo date("Y"); ?> CineVault. All rights reserved.</p>
-    
-    <nav class="navbar">
-        <a href="index.php">Home</a>
-        <a href="movies.php">Page 2</a>
-        <a href="search.php">More</a>
-    </nav>
+    <div class="footer-inner">
+        <div class="footer-logo">Movie Mayhem</div>
+
+        <nav class="footer-nav">
+            <a href="index.php">Home</a>
+            <a href="../pages/select.php">Select</a>
+            <a href="../pages/insert.php">Insert</a>
+            <a href="../pages/update.php">Update</a>
+            <a href="../pages/delete.php">Delete</a>
+        </nav>
+
+        <p class="footer-copy">&copy; <?php echo date("Y"); ?> Movie Mayhem. All rights reserved.</p>
+
+    </div>
+
 </footer>
+</body>
+</html>
